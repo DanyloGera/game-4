3cd1f6bb9a6f3f1c6c9299ad8f93b8ff6156a892
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-19 19:57:37 +0200

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ var text;
 var winningMessage;
 var won = false;
 var currentScore = 0;
-var winningScore = 480;
+var winningScore = 240;
 
 // add collectable items to the game
 function addItems() {
