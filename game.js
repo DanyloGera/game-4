5a4ca0a1bac0a97b0657e30c3faa840dfdae8996
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-21 11:13:31 +0200

    Update game.js

--- game.js
+++ game.js
@@ -96,7 +96,7 @@ function itemHandler(player, item) {
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
-  this.location.href="https://danylogera.github.io/game-3/";
+  document.location.href="https://danylogera.github.io/game-3/";
 }
 
 // setup game when the web page loads
