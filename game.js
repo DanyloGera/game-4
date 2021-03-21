e9c151774a75485c22cc9032914d77d23f878012
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-21 12:04:14 +0200

    Update game.js

--- game.js
+++ game.js
@@ -53,12 +53,12 @@ function addPlatforms() {
   //
   platforms.create(747, 510, 'box');
   platforms.create(747, 455, 'box');
-  platforms.create(747, 400, 'box');
   platforms.create(692, 510, 'box');
   platforms.create(455, 309, 'box2');
   platforms.create(357, 225, 'box3');
   platforms.create(555, 225, 'box3');
   platforms.create(214, 328, 'box4');
+  platforms.create(725, 329, 'box4');
 
   platforms.setAll('body.immovable', true);
 }
